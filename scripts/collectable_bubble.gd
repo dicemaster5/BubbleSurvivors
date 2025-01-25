ab15ad477e13175cdca4868807dcfc3d32749ac3
Dice <dicemaster555@gmail.com>
2025-01-25 17:00:28 +0100

    bubbles can now be connected again

--- scripts/collectable_bubble.gd
+++ scripts/collectable_bubble.gd
@@ -12,18 +12,19 @@ func _process(_delta: float) -> void:
 func on_body_entered(body: Node) -> void:
 	print("Collided with ", body.name)
 	if body is PhysicsBody3D:
-		if body.collision_layer == 1:
+		if body.collision_layer == 2:
 			print("player collided!")
 			connect_to_body(body)
 			return
-		elif body.collision_layer == 16:
+		elif body.collision_layer == 8:
 			print("Bubble collided!")
 			connect_to_body(body)
 			return
+	pop_bubble()
 
 func connect_to_body(body: Node3D) -> void:
 	print("Bubble Connected! to ", body.name)
-	# freeze = true
+	freeze = true
 	reparent.call_deferred(body)
 	is_attached = true
 
