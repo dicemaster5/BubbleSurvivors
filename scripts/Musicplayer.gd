076456141e636a484e674ee26a13775e04229f61
BazoDan <83952478+BazoDan@users.noreply.github.com>
2025-01-26 13:07:50 +0100

    Added Proper music on death
    Yey

--- scripts/Musicplayer.gd
+++ scripts/Musicplayer.gd
@@ -1,9 +1,18 @@
 extends AudioStreamPlayer3D
 
+@export var player: Player
+@export var gameover_music: AudioStream
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	finished.connect(Playnexttrack)
-
+	print("Nuts")
+	player.player_died.connect(stop_music)
+	
+func stop_music():
+	stop()
+	stream = gameover_music
+	play()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func Playnexttrack():
 	play()
