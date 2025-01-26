2e0fd10fddf78cd6003f11046d6fe450b3864d8c
moisturizedfish <moisturizedfish@pm.me>
2025-01-26 11:03:47 +0100

    Implement time formatting, hook up UIs to score

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,15 @@
 extends Node
 
+@onready var score_timer: Timer = $ScoreTimer
+
 @export var player: Player
 @export var game_over_screen: Control
+@export var game_ui: Control
+
+var player_score: int
 
 func _ready() -> void:
-	player.player_died.connect(Callable(game_over_screen.show))
-	pass
+	player.player_died.connect(game_over)
 
 func  _input(event: InputEvent) -> void:
 	if event.is_action_pressed("quick_quit"):
@@ -16,3 +20,12 @@ func  _input(event: InputEvent) -> void:
 
 func _process(_delta: float) -> void:
 	pass
+
+func _on_score_timer_timeout() -> void:
+	player_score += 1
+	game_over_screen.time_score = player_score
+	game_ui.time_score = player_score
+
+func game_over():
+	score_timer.stop()
+	game_over_screen.enable_screen()
